28320593fadc6dc6357728722f07c0946cc7a421
jinseok Bea <115687246+jimseokbea@users.noreply.github.com>
2025-10-20 18:07:54 +0900

    feat: 결과 출력 기능구현

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,2 +1,10 @@
-package calculator.view;public class OutputView {
+package calculator.view;
+
+public class OutputView {
+    private static final String RESULT_PREFIX = "결과 : ";
+
+
+     // 계산 결과를 화면에 출력하며 보여주기
+    public void printResult(int result) {
+        System.out.println(RESULT_PREFIX + result);
 }
