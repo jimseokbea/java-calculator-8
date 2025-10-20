4c383d2734bba303d094e76b441cce4f7a585248
jinseok Bea <115687246+jimseokbea@users.noreply.github.com>
2025-10-20 18:06:25 +0900

    feat: 사용자 입력 기능 구현

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,2 +1,20 @@
-package calculator.view;public class InputView {
+package calculator.view;
+
+public class InputView
+
+     // 사용자 입력을 담당하는 클래스
+
+
+    public class InputView {
+
+        private static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+
+
+         // 사용자로부터 계산할 문자열을 입력받음
+        public String readInput() {
+            System.out.println(INPUT_MESSAGE);
+            return Console.readLine();
+        }
+    }
+
 }
