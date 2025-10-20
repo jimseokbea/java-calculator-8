a58eb3a3b1d035f3ca087bd25fe3e706c3ae0a3f
jinseok Bea <115687246+jimseokbea@users.noreply.github.com>
2025-10-20 18:15:54 +0900

    feat: 프로그램 시작점 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,31 @@
 package calculator;
 
+import calculator.domain.StringCalculator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 필요한 객체들 생성한다
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        StringCalculator calculator = new StringCalculator();
+
+        try {
+            // 1. 입력하기
+            String input = inputView.readInput();
+
+            // 2. 계산하기
+            int result = calculator.calculate(input);
+
+            // 3. 출력하기
+            outputView.printResult(result);
+
+        } catch (IllegalArgumentException e) {
+            // 예외 발생시 그대로 던져서 프로그램 종료한다
+            throw e;
+        }
     }
 }
